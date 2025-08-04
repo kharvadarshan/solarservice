baa013bd515560b85159220da6013d5bbaad90fe
kharvadarshan <150101961+kharvadarshan@users.noreply.github.com>
2025-08-04 21:06:56 +0530

    configuration setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser  = require('body-parser')
+const app = express();
+
+
+
+app.use('/api',);
+app.listen(5000,()=>{
+    console.log("Server is running on Port 5000");
+});
